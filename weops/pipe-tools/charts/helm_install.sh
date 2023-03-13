d3bb829c50591cb024bc6b16e27e9564fc754e3a
zhongming Fan <zhongmingfan@canway.net>
2023-03-13 17:27:40 +0800

    feature: charts

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # 部署监控对象
-object_versions=("5.7". "8.0")
+object_versions=("5.7" "8.0")
 object=mysql
 
 for version in "${object_versions[@]}"; do
-    version_suffix="v${version%%.*}"
+    version_suffix="v${version//./-}"
 
-    helm install redis-standalone-$version_suffix --namespace $object  -f ./values/standalone_values.yaml \
+    helm install redis-standalone-$version_suffix --namespace $object -f ./values/standalone_values.yaml \
     --set image.tag=$version \
     --set $object.podLabels.object_version=$version_suffix \
     ./$object
 done
-

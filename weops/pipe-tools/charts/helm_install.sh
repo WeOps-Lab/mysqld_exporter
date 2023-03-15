9809afb5dc3d49583ec6a660769e245026345730
zhongming Fan <zhongmingfan@canway.net>
2023-03-15 18:08:34 +0800

    feature:  5.5 5.6

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -1,21 +1,34 @@
-#!/bin/bash
-
-# 部署监控对象
-object_versions=("5.7" "8.0.32")
+object_versions=("5.7" "5.6" "5.5" "8.0.32")
 object=mysql
 
 for version in "${object_versions[@]}"; do
     version_suffix="v${version//./-}"
 
-    helm install $object-standalone-$version_suffix --namespace $object -f ./values/mysql_values.yaml \
-    --set image.tag=$version \
-    --set architecture=standalone \
-    --set $object.podLabels.object_version=$version_suffix \
-    ./$object
+    if [[ "$version" == "5.5" || "$version" == "5.6" ]]; then
+      helm install $object-standalone-$version_suffix --namespace $object -f ./values/mysql_values.yaml \
+      --set image.repository=mysql \
+      --set image.tag=$version \
+      --set architecture=standalone \
+      --set $object.podLabels.object_version=$version_suffix \
+      ./$object
 
-    helm install $object-cluster-$version_suffix --namespace $object -f ./values/mysql_values.yaml \
-    --set image.tag=$version \
-    --set architecture=replication \
-    --set $object.podLabels.object_version=$version_suffix \
-    ./$object
+      helm install $object-cluster-$version_suffix --namespace $object -f ./values/mysql_values.yaml \
+      --set image.repository=mysql \
+      --set image.tag=$version \
+      --set architecture=replication \
+      --set $object.podLabels.object_version=$version_suffix \
+      ./$object
+    else
+      helm install $object-standalone-$version_suffix --namespace $object -f ./values/mysql_values.yaml \
+      --set image.tag=$version \
+      --set architecture=standalone \
+      --set $object.podLabels.object_version=$version_suffix \
+      ./$object
+
+      helm install $object-cluster-$version_suffix --namespace $object -f ./values/mysql_values.yaml \
+      --set image.tag=$version \
+      --set architecture=replication \
+      --set $object.podLabels.object_version=$version_suffix \
+      ./$object
+    fi
 done

021908623583670fd0403af3f13a785a7a15d32a
zhongming Fan <zhongmingfan@canway.net>
2023-03-15 13:45:41 +0800

    cluster charts

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for version in v5-7 v8-0; do
-  output_file="standalone_${version}.yaml"
-  sed "s/{{VERSION}}/${version}/g" standalone.tpl > ../standalone/${output_file}
+for version in v5-7 v8-0-32; do
+  standalone_output_file="standalone_${version}.yaml"
+  sed "s/{{VERSION}}/${version}/g" standalone.tpl > ../standalone/${standalone_output_file}
+
+  cluster_output_file="cluster_${version}.yaml"
+  sed "s/{{VERSION}}/${version}/g" cluster.tpl > ../cluster/${cluster_output_file}
 done

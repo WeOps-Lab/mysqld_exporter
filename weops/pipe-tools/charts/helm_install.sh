fc0aa773bfbf1b7e4ac68997fecfcfdec65073ac
zhongming Fan <zhongmingfan@canway.net>
2023-03-20 09:32:21 +0800

    feature: helm install mariadb

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -1,25 +1,41 @@
-object_versions=("5.7" "5.6" "5.5" "8.0.32")
-object=mysql
+#!/bin/bash
 
-for version in "${object_versions[@]}"; do
+function install_object {
+  local object=$1
+  local versions=("${!2}")
+
+  for version in "${versions[@]}"; do
     version_suffix="v${version//./-}"
 
     if [[ "$version" == "5.5" || "$version" == "5.6" ]]; then
-      helm install $object-standalone-$version_suffix --namespace $object -f ./values/mysql_values.yaml \
-      --set imageTag=$version \
-      ./mysql
+      helm install ${object}-standalone-${version_suffix} --namespace ${object} \
+        -f ./values/mysql_values.yaml \
+        --set imageTag=${version} \
+        ./mysql
 
     else
-      helm install $object-standalone-$version_suffix --namespace $object -f ./values/bitnami_mysql_values.yaml \
-      --set image.tag=$version \
-      --set architecture=standalone \
-      --set $object.podLabels.object_version=$version_suffix \
-      ./bitnami-mysql
+      helm install ${object}-standalone-${version_suffix} --namespace ${object} \
+        -f bitnami_values.yaml \
+        --set image.tag=${version} \
+        --set architecture=standalone \
+        --set ${object}.podLabels.object_version=${version_suffix} \
+        ./bitnami-${object}
 
-      helm install $object-cluster-$version_suffix --namespace $object -f ./values/bitnami_mysql_values.yaml \
-      --set image.tag=$version \
-      --set architecture=replication \
-      --set $object.podLabels.object_version=$version_suffix \
-      ./bitnami-mysql
+      helm install ${object}-cluster-${version_suffix} --namespace ${object} \
+        -f bitnami_values.yaml \
+        --set image.tag=${version} \
+        --set architecture=replication \
+        --set ${object}.podLabels.object_version=${version_suffix} \
+        ./bitnami-${object}
     fi
-done
+  done
+}
+
+object1_versions=("5.7" "5.6" "5.5" "8.0.32")
+object1=mysql
+
+object2_versions=("10.3" "10.4" "10.5" "10.6" "10.7" "10.8" "10.9")
+object2=mariadb
+
+install_object ${object1} object1_versions[@]
+install_object ${object2} object2_versions[@]
